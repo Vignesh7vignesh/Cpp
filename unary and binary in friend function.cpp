2bfbbeeb956f259d51c05fdd9c1dd90c283d28c5
Vignesh7vignesh <vigneshchintalapudi777@gmail.com>
2025-11-05 19:27:48 +0530

    Create unary and binary in friend function.cpp

---
+++ unary and binary in friend function.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+using namespace std; 
+class Number{ 
+int value; 
+public: 
+Number(int v=0){value=v;} 
+friend Number operator++(Number &n); 
+friend Number operator+(Number n1, Number n2); 
+void display(){cout<<"Value: "<<value<<endl;} 
+}; 
+Number operator++(Number &n){n.value++; return n;} 
+Number operator+(Number n1, Number n2){return Number(n1.value+n2.value);} 
+int main(){ 
+Number n1(5),n2(10),n3; 
+cout<<"Before Unary Operation:\n"; n1.display(); 
+++n1; 
+cout<<"After Unary Operation (++n1):\n"; n1.display(); 
+n3=n1+n2; 
+cout<<"After Binary Operation (n1 + n2):\n"; n3.display(); 
+return 0; 
+} 
